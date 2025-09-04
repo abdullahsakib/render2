7f2a77ebe918c656fb2dbc2bb1a5f86a55c2d751
abdullahsakib <54322794+abdullahsakib@users.noreply.github.com>
2025-09-04 17:37:54 +0600

    Update vite.config.js
    old

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://django-backend.onrender.com",
+      "/api": "http://localhost:8000",
     },
   },
 });
